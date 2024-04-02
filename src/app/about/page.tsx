d227017cc9828f1b30a0ef1c953a8d49e414bbaf
JosephKeswet <josephkeswet@gmail.com>
2024-04-02 20:31:54 +0100

    chore:adding required props

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -43,6 +43,7 @@ const page = (props: Props) => {
 					<RevealHorizontal>
 						<div className="">
 							<GameCard
+								tag="Shooter | Top down"
 								gameName="RaidShooter"
 								img="/assets/RaidShooter.png"
 								content={`Blast through enemy infested rooms, collect deadly weapons and upgrade your arsenal in Raid Shooter, the top down shooter where you unlock guns, characters, and pure action.`}
@@ -52,6 +53,7 @@ const page = (props: Props) => {
 					<RevealHorizontal>
 						<div className="mt-4">
 							<GameCard
+								tag="Hypercasual"
 								gameName="Space hazards & Asteroid Redirection Program"
 								content="Take control of a specialized spacecraft tasked with safeguarding a rocket from incoming waves of space debris."
 								img="/assets/Sharp.jpg"
@@ -64,6 +66,7 @@ const page = (props: Props) => {
 								gameName="Drop Ball"
 								content="Test your anticipation skills as you predict the ball's  trajectory and craft platforms to safely guide it into the cup."
 								img="/assets/DropBall.png"
+								tag="Singleplayer"
 							/>
 						</div>
 					</RevealHorizontal>
